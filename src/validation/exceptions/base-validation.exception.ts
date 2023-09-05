fd85b5896cdedffb2b0f525d7bd66a0dc0f54ea6
Jonah Snider <jonah@jonahsnider.com>
2023-09-05 09:43:51 -0700

    feat: stop requiring http module in BaseValidationException

--- src/validation/exceptions/base-validation.exception.ts
+++ src/validation/exceptions/base-validation.exception.ts
@@ -1,8 +1,8 @@
-import { STATUS_CODES } from 'http';
 import { NextResponse } from 'next/server.js';
 import { TO_RESPONSE } from '../../constants.js';
 import type { ValidationExceptionSchema } from '../dtos/exception.dto.js';
 import type { ExceptionCode } from '../enums/exceptions.enum.js';
+import { Http } from '@jonahsnider/util';
 
 /**
  * The base class that exceptions thrown during validation inherit from.
@@ -12,14 +12,14 @@ import type { ExceptionCode } from '../enums/exceptions.enum.js';
 export class BaseValidationException extends Error implements ValidationExceptionSchema {
 	readonly error: string;
 	readonly code: ExceptionCode | undefined;
-	readonly statusCode: number;
+	readonly statusCode: Http.Status;
 
-	constructor(message: string, statusCode: number, code: ExceptionCode | undefined) {
+	constructor(message: string, statusCode: Http.Status, code: ExceptionCode | undefined) {
 		super(message);
 
 		this.code = code;
 		this.statusCode = statusCode;
-		this.error = STATUS_CODES[statusCode] ?? BaseValidationException.name;
+		this.error = Http.StatusName[statusCode] ?? BaseValidationException.name;
 	}
 
 	[TO_RESPONSE](): NextResponse<ValidationExceptionSchema> {
