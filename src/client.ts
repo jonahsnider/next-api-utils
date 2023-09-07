7a32264909663d8a0e5d1688dbb88a55584821c3
Jonah Snider <jonah@jonahsnider.com>
2023-09-06 22:56:06 -0700

    refactor: use biome-ignore

--- src/client.ts
+++ src/client.ts
@@ -2,5 +2,5 @@ export * from './constants.js';
 export * from './dtos/query-boolean.dto.js';
 export * from './interfaces/next-route-handler-context.interface.js';
 export * from './interfaces/next-route-handler.interface.js';
-// rome-ignore lint/nursery/useImportRestrictions: This is the entry file for the validation module
+// biome-ignore lint/nursery/useImportRestrictions: This is the entry file for the validation module
 export * from './validation/client.js';
